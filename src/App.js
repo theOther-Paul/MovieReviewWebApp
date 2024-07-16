d0a92bd22f075828ad16891bcb96c7696ecc74bb
theOther-Paul <paulflorentin.pf@gmail.com>
2024-07-16 12:48:28 +0300

    Frontend

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { Route, Routes } from "react-router-dom";
 import './App.css';
 import api from './api/axiosConfig';
+import Layout from './components/Layout';
+import Home from './components/home/Home';
 
 function App() {
   const [movies, setMovies] =useState();
@@ -30,6 +33,13 @@ function App() {
   return (
     <div className="App">
       
+<Routes>
+  <Route path="/" element={<Layout />}>
+    <Route path="/" element={<Home movies={movies} />}></Route>
+  
+  </Route>
+</Routes>
+
     </div>
   );
 }
